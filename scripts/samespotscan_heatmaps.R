d336f61f57d4c965f0927c346b22cc710a094ad4
Kyle Lima <klima@schoodicinstitue.org>
2022-04-30 09:03:18 -0400

    developing heat map

--- scripts/samespotscan_heatmaps.R
+++ scripts/samespotscan_heatmaps.R
@@ -6,17 +6,35 @@
 ####           Packages Required              ####
 #------------------------------------------------#
 
-library(tidyverse)
-library(tidyr)
-library(dplyr)
+library(spectrolab)
+library(gplots)
+library(vegan)
+
+select <- dplyr::select
 
 
 
 #------------------------------------------------#
-####           Data Manipulation              ####
+####            Read in spectra               ####
 #------------------------------------------------#
 
+#Read in the spectra from the multi scan per point datasets
+caribou_spectra = read_spectra(path = 'data/pvy_scan_20220429/caribou_multi')
+lamoka_spectra = read_spectra(path = 'data/pvy_scan_20220429/lamoka_multi')
 
+
+
+#------------------------------------------------#
+####              Heat map viz                ####
+#------------------------------------------------#
+
+#Create heat map
+heatmap.2(as.matrix(decostand(caribou_spectra, "standardize", MARGIN = 2), 
+  breaks = c(seq(-2, 2, by = 0.1)), 
+  dendrogram = "row", 
+  trace = "none", 
+  Colv = TRUE,
+  col = "heat.colors"))
 
 
 
