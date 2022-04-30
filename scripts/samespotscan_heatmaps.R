c15b2d81225c1c9b4ed970cba04824d1a6d547b7
Kyle Lima <klima@schoodicinstitue.org>
2022-04-30 08:14:39 -0400

    cloning and making KAL branch

---
+++ scripts/samespotscan_heatmaps.R
@@ -0,0 +1,30 @@
+#Heat maps for same spot scans
+#UMO Genome Elimination Lab - Schoodic Institute at Acadia National Park
+#Kyle Lima 2021
+
+#------------------------------------------------#
+####           Packages Required              ####
+#------------------------------------------------#
+
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+
+
+
+#------------------------------------------------#
+####           Data Manipulation              ####
+#------------------------------------------------#
+
+
+
+
+
+
+
+
+
+
+
+
+
